f89e8f14dad92fc5b0d07f172a21e170b921dc11
Ruslan <rus.volskiy@gmail.com>
2023-02-04 22:16:17 +0200

    asd

---
+++ first-attempt/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+
+ 
+
+    private static void Main(string[] args)
+    {
+        Random r = new Random();
+
+        int winNum = r.Next(0, 100);
+
+        bool win = false;
+
+        do
+        {
+            Console.WriteLine("Gues the number from 0 to 100:");
+
+            string guesNum = Console.ReadLine();
+
+            int gues = int.Parse(guesNum);
+
+            if (gues > winNum)
+            {
+                Console.WriteLine("It's too high, gues lower num");
+
+            }
+            else if (gues < winNum)
+            {
+                Console.WriteLine("It's too low, gues higher num");
+            }
+            else if (gues == winNum)
+            {
+                Console.WriteLine("You won");
+                win = true;
+            }
+
+        } while (win == false);
+
+    }
+
+    
+}
+
---
+++ first-attempt/Something.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace first_attempt
+{
+	public class Something
+	{
+		public int Sum(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
+
